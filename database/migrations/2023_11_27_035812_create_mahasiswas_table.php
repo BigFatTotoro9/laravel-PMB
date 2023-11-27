fc5ba8dfe4f7b261d050db0f6a07bd758f2e5832
zakyyl <zaky24112003@gmail.com>
2023-11-27 11:08:37 +0700

    tambah database

--- database/migrations/2023_11_27_035812_create_mahasiswas_table.php
+++ database/migrations/2023_11_27_035812_create_mahasiswas_table.php
@@ -14,15 +14,10 @@ return new class extends Migration
         Schema::create('mahasiswas', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('agama');
-            $table->string('jenis_kelamin');
-            $table->string('no_hp');
+            $table->string('jenkel');
+            $table->date('tanggal_lahir');
             $table->string('asal_sma');
-            $table->year('tahun_lulus');
-            $table->string('alamat');
-            $table->string('provinsi');
-            $table->string('kabupaten');
-            $table->string('desa');
+            $table->date('tahun_lulus');
             $table->timestamps();
         });
     }
