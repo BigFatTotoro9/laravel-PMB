dc3e0722e16c19b4aa00d10bc8e73114a8ef8236
BigFatTotoro9 <fadhilahrasyid9@gmail.com>
2023-11-30 14:20:18 +0700

    repair

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,23 @@
-<<<<<<< HEAD
-@extends('layouts.medilab')
-=======
-@extends('layouts.app')
->>>>>>> 8b19830a147d8124a4acf031e24d0974c6a29613
+ @extends('layouts.medilab')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+ @section('content')
+     <div class="container">
+         <div class="row justify-content-center">
+             <div class="col-md-8">
+                 <div class="card">
+                     <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                     <div class="card-body">
+                         @if (session('status'))
+                             <div class="alert alert-success" role="alert">
+                                 {{ session('status') }}
+                             </div>
+                         @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                         {{ __('You are logged in!') }}
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use App\Models\Mahasiswa;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\JurusanController;
-=======
 use App\Http\Controllers\MahasiswaController;
->>>>>>> 8b19830a147d8124a4acf031e24d0974c6a29613
 
 /*
 |--------------------------------------------------------------------------
@@ -23,16 +20,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource(name : 'jurusan', controller: JurusanController::class);
-=======
 Route::resource('mahasiswa', MahasiswaController::class);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
->>>>>>> 8b19830a147d8124a4acf031e24d0974c6a29613
