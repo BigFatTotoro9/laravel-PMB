54e4c52ebd8c080379b00dbefdfc7cfd648be2a4
BigFatTotoro9 <fadhilahrasyid9@gmail.com>
2023-11-30 11:01:01 +0700

    add mahasiswa form

---
+++ resources/views/mahasiswa_form.blade.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    {{ $judul }}
+                </div>
+                <div class="card-body">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
