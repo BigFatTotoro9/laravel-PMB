0c0a11564dd9ee9fb8d00bbbb208e7b6299c0461
zakyyl <zaky24112003@gmail.com>
2023-12-04 11:10:43 +0700

    zakuy

--- database/migrations/2023_11_27_035718_create_jurusans_table.php
+++ database/migrations/2023_11_27_035718_create_jurusans_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('jenis_jurusan');
             $table->string('jumlah_pendaftar')->default('0');
+            $table->string('deskripsi');
             $table->timestamps();
         });
     }
--- resources/views/jurusan_index.blade.php
+++ resources/views/jurusan_index.blade.php
@@ -14,7 +14,8 @@
                     <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    <a href="{{ route('jurusan.show', $item->id) }}" class="small-box-footer">More info<i
+                            class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
         @endforeach
